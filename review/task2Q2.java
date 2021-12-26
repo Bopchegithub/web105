fdbdf54206156bfee5ec9f427f02aa09be3400c0
Bopchegithub <96582690+Bopchegithub@users.noreply.github.com>
2021-12-26 16:11:37 +0530

    Add files via upload

---
+++ review/task2Q2.java
@@ -0,0 +1,14 @@
+package palakjava;
+
+public class task2Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(20&15);
+		System.out.println(20|15);
+		System.out.println(30&20);
+		System.out.println(30|20);
+System.out.println(80&25);
+System.out.println(80|25);
+}
+	}
