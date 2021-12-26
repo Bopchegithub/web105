89c08eb85606e2a4977f65091c630094298a7dc0
Bopchegithub <96582690+Bopchegithub@users.noreply.github.com>
2021-12-26 16:07:08 +0530

    Add files via upload

---
+++ review/asad.java
@@ -0,0 +1,20 @@
+package palakjava;
+
+public class asad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=60,b=30,c,d,e,f,g;
+c=a+b;
+d=a-b;
+e=a*b;
+f=c/2;
+g=a/b;
+System.out.println("Addition is" +c);
+System.out.println("Substraction is" +d);
+System.out.println("Multiplication is" +e);
+System.out.println("Average is" +f);
+System.out.println("Division is"+g);
+	}
+
+}
