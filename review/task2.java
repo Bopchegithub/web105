c3b78c242840343612f0ee39a56a42ce87e51277
Bopchegithub <96582690+Bopchegithub@users.noreply.github.com>
2021-12-26 16:16:39 +0530

    Add files via upload

---
+++ review/task2.java
@@ -0,0 +1,21 @@
+package palakjava;
+
+public class task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int id=101;
+String name="jones";
+char gender='M';
+long  number=1122334455;
+String email="customer1@gmail.com";
+System.out.println("Customer Details");
+System.out.println("- - - - - - - - - - - - -");
+System.out.println("CustomerName="+name);
+System.out.println("Customerid="+id);
+System.out.println("CustomerGender="+gender);
+System.out.println("CustomerNumber="+number);
+System.out.println("CustomerEmail"+email);
+	}
+
+}
