c7855f74861e68c55669d555c282cc8971a1d4c1
Bopchegithub <96582690+Bopchegithub@users.noreply.github.com>
2022-01-09 20:22:25 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,17 @@
+package palakjava;
+
+import java.util.Scanner;
+
+public class table {
+
+		public static void main(String[] args) {
+
+			 Scanner in = new Scanner(System.in);
+			 System.out.println("enter number");
+			 int n =in.nextInt();
+			 for(int i=1;i<=10;i++) {
+				 System.out.println(+n*i);
+	}
+			 
+}
+}
