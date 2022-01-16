5ad71e28ad35b9fff57988d3148bb6fe5f6fd544
Bopchegithub <96582690+Bopchegithub@users.noreply.github.com>
2022-01-16 23:18:02 +0530

    Add files via upload

---
+++ CustomerTest.java
@@ -0,0 +1,10 @@
+package com.xyz.app1.test;
+
+public class CustomerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("new class new package");
+	}
+
+}
