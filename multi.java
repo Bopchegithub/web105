74293cf525f65d39c03849576f71131e1d823901
Bopchegithub <96582690+Bopchegithub@users.noreply.github.com>
2022-01-23 23:06:18 +0530

    Add files via upload

--- multi.java
+++ multi.java
@@ -1,6 +1,6 @@
 package palakjava;
 
-public class multi {
+ class multi {
 void sound() {
 	System.out.println("sound........");
 }
@@ -8,33 +8,5 @@ void picture() {
 	System.out.println("picture........");
 }
 }
-class Threedtv extends multi{
-	void threed() {
-		
-			System.out.println("three D........");
-		
-	}
-	void dolbyaudio() {
-		System.out.println("Dol by Audio........");
-	}
-	class smarttv extends Threedtv{
-		void internet() {
-			System.out.println("internet........");
-		}
-		void fullhd() {
-			System.out.println("HD........");
-		}
-	}
-	class inheritancedemo{
-	public static void main(String[] args) {
-		smarttv m= new smarttv();
-		m.sound();
-		m.picture();
-		m.threed();
-		m.dolbyaudio();
-		m.internet();
-m.fullhd();
-	}
 
-	}
-}
+
